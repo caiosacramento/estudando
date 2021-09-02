6c2a61ad4936e62ac0676efe7d49a23a2dee0401
Caio sacramento <caiosacramento@outlook.com>
2021-09-01 23:02:35 -0300

    usando panda e yahoofinancials

--- treinando.py
+++ treinando.py
@@ -1,6 +1,13 @@
-import panda as pd 
-print("consulta cep")
-print()
+import pandas as pd  
+from yahoofinancials import YahooFinancials
+yahoo_financials = YahooFinancials('MSFT')
 
-cep_input = input('digite o cep')
-print(cep_input)
+data = yahoo_financials.get_historical_price_data(start_date='2000-01-01', 
+                                                  end_date='2019-12-31', 
+                                                  time_interval='weekly')
+
+tsla_df = pd.DataFrame(data['MSFT']['prices'])
+tsla_df = tsla_df.drop('date', axis=1).set_index('formatted_date')
+tsla_df.head()
+print(data)
+print(tsla_df)
